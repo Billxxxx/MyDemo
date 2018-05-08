9bb1debe30fc7e4d0fe239a52372f32ed86f9b56
BillWang <76603338@qq.com>
2018-05-08 14:45:59 +0800

    swipebaseactivity

---
+++ app/src/main/java/com/bill/billdemo/BaseActivity.java
@@ -0,0 +1,72 @@
+package com.bill.billdemo;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import me.imid.swipebacklayout.lib.SwipeBackLayout;
+import me.imid.swipebacklayout.lib.Utils;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivityBase;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivityHelper;
+
+public abstract class BaseActivity extends AppCompatActivity implements SwipeBackActivityBase {
+    private SwipeBackActivityHelper mHelper;
+    private SwipeBackLayout mSwipeBackLayout;
+
+    protected boolean enableSwipeBack() {
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (enableSwipeBack()) {
+            mHelper = new SwipeBackActivityHelper(this);
+            mHelper.onActivityCreate();
+            swpieBackInit();
+        }
+    }
+
+    private void swpieBackInit() {
+        mSwipeBackLayout = getSwipeBackLayout();
+        //设置滑动方向，可设置EDGE_LEFT, EDGE_RIGHT, EDGE_ALL, EDGE_BOTTOM
+        mSwipeBackLayout.setEdgeTrackingEnabled(SwipeBackLayout.EDGE_LEFT);
+        //mSwipeBackLayout.setEdgeSize(200);//滑动删除的效果只能从边界滑动才有效果，如果要扩大touch的范围，可以调用这个方法
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        if (enableSwipeBack()) {
+            mHelper.onPostCreate();
+        }
+    }
+
+    @Override
+    public View findViewById(int id) {
+        if (enableSwipeBack()) {
+            View v = super.findViewById(id);
+            if (v == null && mHelper != null)
+                return mHelper.findViewById(id);
+            return v;
+        } else
+            return super.findViewById(id);
+    }
+
+    @Override
+    public SwipeBackLayout getSwipeBackLayout() {
+        return mHelper.getSwipeBackLayout();
+    }
+
+    @Override
+    public void setSwipeBackEnable(boolean enable) {
+        getSwipeBackLayout().setEnableGesture(enable);
+    }
+
+    @Override
+    public void scrollToFinishActivity() {
+        Utils.convertActivityToTranslucent(this);
+        getSwipeBackLayout().scrollToFinishActivity();
+    }
+}
