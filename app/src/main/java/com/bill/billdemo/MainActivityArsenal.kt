b5a89d5bb7f292af27162e47d8d8afec38c78e87
BillWang <76603338@qq.com>
2018-05-10 10:15:03 +0800

    引入BaseRecyclerViewAdapterHelper

--- app/src/main/java/com/bill/billdemo/MainActivityArsenal.kt
+++ app/src/main/java/com/bill/billdemo/MainActivityArsenal.kt
@@ -1,23 +1,43 @@
 package com.bill.billdemo
 
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.arsenal.bill.ArsenalBaseActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_adapter.*
+import java.util.*
 
 class MainActivityArsenal : ArsenalBaseActivity() {
     lateinit var mRecyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adapter)
-        mRecyclerView = recycler_view
+        mRecyclerView = rv_list
         mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setLayoutManager(  LinearLayoutManager(this));
+
+        val data = getMultipleItemData()
+        val multipleItemAdapter = MultipleItemQuickAdapter(this, data)
+        val manager = GridLayoutManager(this, 4)
+        mRecyclerView.layoutManager = manager
+        multipleItemAdapter.setSpanSizeLookup { gridLayoutManager, position -> data.get(position).getSpanSize() }
+        mRecyclerView.adapter = multipleItemAdapter
 
     }
 
     override fun enableSwipeBack(): Boolean {
         return false
+    }
+
+    fun getMultipleItemData(): List<MultipleItem> {
+        val list = ArrayList<MultipleItem>()
+        for (i in 0..4) {
+            list.add(MultipleItem(MultipleItem.IMG, MultipleItem.IMG_SPAN_SIZE))
+            list.add(MultipleItem(MultipleItem.TEXT, MultipleItem.TEXT_SPAN_SIZE, "CymChad"))
+            list.add(MultipleItem(MultipleItem.IMG_TEXT, MultipleItem.IMG_TEXT_SPAN_SIZE))
+            list.add(MultipleItem(MultipleItem.IMG_TEXT, MultipleItem.IMG_TEXT_SPAN_SIZE_MIN))
+            list.add(MultipleItem(MultipleItem.IMG_TEXT, MultipleItem.IMG_TEXT_SPAN_SIZE_MIN))
+        }
+
+        return list
     }
 }
