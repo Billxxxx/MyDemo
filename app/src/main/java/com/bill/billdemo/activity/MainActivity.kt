6865ccf2c6aa6a6cce7c93d577989378627a49a3
BillWang <76603338@qq.com>
2018-05-14 14:01:18 +0800

    使用ARouter

--- app/src/main/java/com/bill/billdemo/activity/MainActivity.kt
+++ app/src/main/java/com/bill/billdemo/activity/MainActivity.kt
@@ -2,6 +2,8 @@ package com.bill.billdemo.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.alibaba.android.arouter.launcher.ARouter
+import com.bill.billdemo.BuildConfig
 import com.bill.billdemo.R
 import kotlinx.android.synthetic.main.ac_main.*
 
@@ -9,9 +11,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ac_main)
+
+        if (BuildConfig.DEBUG) {           // 这两行必须写在init之前，否则这些配置在init过程中将无效
+            ARouter.openLog();     // 打印日志
+            ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+        }
+        ARouter.init(application); // 尽可能早，推荐在Application中初始化
+
         list_btn.setOnClickListener {
-//            ARouter.getInstance().
-//            ARouter.getInstance().build("/base/list").navigation();
+            ARouter.getInstance().build("/test/testactivity").navigation();
         }
     }
 }
--- app/src/main/java/com/bill/billdemo/activity/TestActivity.java
+++ app/src/main/java/com/bill/billdemo/activity/TestActivity.java
@@ -3,10 +3,11 @@ package com.bill.billdemo.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.alibaba.android.arouter.facade.annotation.Route;
 import com.bill.billdemo.R;
 
-//@Route(path = "/test/testactivity")
-public class testActivity extends AppCompatActivity {
+@Route(path = "/test/testactivity")
+public class TestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
