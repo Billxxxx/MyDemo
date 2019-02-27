c6e9780cd2846dd7028f492b64da053a90347343
BillWang <76603338@qq.com>
2019-02-27 14:29:13 +0800

    no message

--- app/src/test/java/com/bill/billdemo/ExampleUnitTest.java
+++ app/src/test/java/com/bill/billdemo/ExampleUnitTest.java
@@ -1,8 +1,7 @@
 package com.bill.billdemo;
 
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +9,37 @@ import static org.junit.Assert.assertEquals;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-  @Test
-  public void addition_isCorrect() throws Exception {
-    assertEquals(4, 2 + 2);
-  }
+    @Test
+    public void addition_isCorrect() throws Exception {
+        Parent parent = new Parent();
+        parent.method();
+        parent.staticMethod();
+
+        Child child = new Child();
+        child.method();
+        child.staticMethod();
+    }
+}
+
+
+class Parent {
+
+    public static void staticMethod() {
+        System.out.println("Parent Static Method");
+    }
+
+    public void method() {
+        System.out.println("Parent Method");
+    }
+}
+
+class Child extends Parent {
+
+    public static void staticMethod() {
+        System.out.println("Child Static Method");
+    }
+
+    public void method() {
+        System.out.println("Child Method");
+    }
 }
